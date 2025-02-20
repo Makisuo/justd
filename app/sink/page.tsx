29369e9c0930b9cec174be71be3bea83666390b7
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-02-20 20:42:46 +0700

    number field

--- app/sink/page.tsx
+++ app/sink/page.tsx
@@ -1,9 +1,5 @@
-import { NumberField } from "ui"
-
 export default function Page() {
   return (
-    <div className="p-10">
-      <NumberField label="Number Field" name="number" />
-    </div>
+    <div/>
   )
 }
