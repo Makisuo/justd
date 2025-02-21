bece6b981811d786d4d1d55d260e1e4fa23d18cf
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-02-21 08:46:52 +0700

    fix popover

--- app/sink/page.tsx
+++ app/sink/page.tsx
@@ -1,13 +1,5 @@
-import MenuBasicDemo from "@/components/docs/collections/menu/menu-basic-demo"
-import PopoverDemo from "@/components/docs/overlays/popover/popover-demo"
-
 export default function Page() {
   return (
-    <div className="grid grid-cols-4 gap-4">
-      <MenuBasicDemo />
-      <PopoverDemo />
-      {/*<ComboBoxDemo/>*/}
-      {/*<SelectDemo/>*/}
-    </div>
+    <div/>
   )
 }
