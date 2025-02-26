8e7dd31ed8308f907fe3b6a256846b7c388f8cc7
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-02-26 07:19:22 +0700

    refactor calendar

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { CalendarContext, useSlottedContext } from "react-aria-components"
+import { CalendarStateContext } from "react-aria-components"
 
 import { IconChevronLgLeft, IconChevronLgRight } from "justd-icons"
 import type { CalendarProps as CalendarPrimitiveProps, DateValue } from "react-aria-components"
@@ -19,7 +19,10 @@ import {
 
 import { Select } from "@/components/ui/select"
 import { cn } from "@/utils/classes"
-import { getLocalTimeZone, today } from "@internationalized/date"
+import { type CalendarDate, getLocalTimeZone, today } from "@internationalized/date"
+import { useDateFormatter } from "@react-aria/i18n"
+import type { CalendarState } from "@react-stately/calendar"
+import { use } from "react"
 import { Button } from "./button"
 
 interface CalendarProps<T extends DateValue>
@@ -66,65 +69,20 @@ const Calendar = <T extends DateValue>({ errorMessage, className, ...props }: Ca
   )
 }
 
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-]
-
 const years = Array.from({ length: new Date().getFullYear() - 1980 + 1 }, (_, i) => 1980 + i)
 
-const CalendarHeader = ({ className, ...props }: React.ComponentProps<'header'>) => {
-  const { direction } = useLocale()
-  const context = useSlottedContext(CalendarContext)!
-  const now = today(getLocalTimeZone())
+const CalendarHeader = ({ className, ...props }: React.ComponentProps<"header">) => {
+  const { direction, locale } = useLocale()
+  const state = use(CalendarStateContext)!
+
   return (
     <header
       data-slot="calendar-header"
       className={cn("flex w-full justify-center gap-2 px-1 pt-1 pb-5 sm:pb-4", className)}
       {...props}
     >
-      <Select
-        aria-label="Select month"
-        selectedKey={context?.value?.month ?? new Date().getMonth() + 1}
-        onSelectionChange={(v) => {
-          context?.onChange?.(now.set({ month: v as number }))
-        }}
-      >
-        <Select.Trigger className="h-8 text-xs" />
-        <Select.List>
-          {months.map((month, index) => (
-            <Select.Option key={index} id={index + 1}>
-              <Select.Label>{month}</Select.Label>
-            </Select.Option>
-          ))}
-        </Select.List>
-      </Select>
-      <Select
-        aria-label="Select year"
-        selectedKey={context?.value?.year.toString() ?? new Date().getFullYear().toString()}
-        onSelectionChange={(v) => {
-          context?.onChange?.(now.set({ year: v as number }))
-        }}
-      >
-        <Select.Trigger className="h-8 text-xs tabular-nums tracking-tight" />
-        <Select.List>
-          {years.map((year) => (
-            <Select.Option key={year} id={year.toString()}>
-              <Select.Label className="tabular-nums">{year}</Select.Label>
-            </Select.Option>
-          ))}
-        </Select.List>
-      </Select>
+      <SelectMonth state={state} />
+      <SelectYear state={state} />
       <Heading
         className={cn(
           "sr-only mr-2 flex-1 text-left font-medium text-muted-fg sm:text-sm",
@@ -152,6 +110,74 @@ const CalendarHeader = ({ className, ...props }: React.ComponentProps<'header'>)
         </Button>
       </div>
     </header>
+  )
+}
+
+const SelectMonth = ({ state }: { state: CalendarState }) => {
+  const months = []
+
+  const formatter = useDateFormatter({
+    month: "long",
+    timeZone: state.timeZone,
+  })
+
+  const numMonths = state.focusedDate.calendar.getMonthsInYear(state.focusedDate)
+  for (let i = 1; i <= numMonths; i++) {
+    const date = state.focusedDate.set({ month: i })
+    months.push(formatter.format(date.toDate(state.timeZone)))
+  }
+  return (
+    <Select
+      aria-label="Select month"
+      selectedKey={state.focusedDate.month.toString() ?? (new Date().getMonth() + 1).toString()}
+      onSelectionChange={(value) => {
+        state.setFocusedDate(state.focusedDate.set({ month: Number(value) }))
+      }}
+    >
+      <Select.Trigger className="h-8 text-xs" />
+      <Select.List>
+        {months.map((month, index) => (
+          <Select.Option key={index} id={(index + 1).toString()} textValue={month}>
+            <Select.Label>{month}</Select.Label>
+          </Select.Option>
+        ))}
+      </Select.List>
+    </Select>
+  )
+}
+
+const SelectYear = ({ state }: { state: CalendarState }) => {
+  const years: { value: CalendarDate; formatted: string }[] = []
+  const formatter = useDateFormatter({
+    year: "numeric",
+    timeZone: state.timeZone,
+  })
+
+  for (let i = -20; i <= 20; i++) {
+    const date = state.focusedDate.add({ years: i })
+    years.push({
+      value: date,
+      formatted: formatter.format(date.toDate(state.timeZone)),
+    })
+  }
+  return (
+    <Select
+      aria-label="Select year"
+      selectedKey={20}
+      onSelectionChange={(value) => {
+        // @ts-expect-error
+        state.setFocusedDate(years[Number(value)]?.value)
+      }}
+    >
+      <Select.Trigger className="h-8 text-xs" />
+      <Select.List>
+        {years.map((year, i) => (
+          <Select.Option key={i} id={i} textValue={year.formatted}>
+            <Select.Label>{year.formatted}</Select.Label>
+          </Select.Option>
+        ))}
+      </Select.List>
+    </Select>
   )
 }
 
